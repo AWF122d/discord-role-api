7e3392c13e3f697b4434b5e268523c7c6bd12d22
AWF122d <xfrankieeroblox@gmail.com>
2026-04-22 19:06:55 +0100

    Delete User.js

--- User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-  discordId: { type: String, required: true },
-  chosenRole: { type: String, default: "None" },
-  dashboardRole: { type: String, default: "None" }
-});
-
-module.exports = mongoose.model("User", UserSchema);
